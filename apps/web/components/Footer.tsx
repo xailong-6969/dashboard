3b749f3838a51fccac94e75429e0042080df6e36
Goku0007 <77104674+xailong-6969@users.noreply.github.com>
2026-01-25 11:06:06 +0530

    Remove old apps folder

--- apps/web/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="w-full border-t border-neutral-800 py-4 text-center text-xs text-neutral-400">
-      <div>
-        Maintained by xailong_6969
-      </div>
-    </footer>
-  );
-}
